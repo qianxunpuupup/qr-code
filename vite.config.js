8ec943c172c3bd5c8cc7e9b58cebdbadff484786
qianxunpuupup <wangh9696@163.com>
2026-08-20 09:41:40 +0800

    修改编译打包

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,28 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-export default defineConfig({ plugins: [vue()] });
+export default defineConfig({
+  server: {
+    port: 3000,
+    open: true,
+  },
+
+  plugins: [vue()],
+
+  build: {
+    target: "es2015",
+    outDir: "QRCode-release",
+    chunkSizeWarningLimit: 2000,
+    terserOptions: {
+      compress: {
+        drop_console: true, // 生产环境去除 console
+        drop_debugger: true, // 生产环境去除 debugger
+      },
+    },
+
+    dynamicImportVarsOptions: {
+      warnOnError: true,
+      exclude: [],
+      include: ["src/views/**/*.vue"],
+    },
+  },
+});
