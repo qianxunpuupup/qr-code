7ca499a73e8d40d9e4851dd43aa416627a4b308e
qianxunpuupup <wangh9696@163.com>
2026-08-20 11:25:50 +0800

    修改编译打包

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,8 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 export default defineConfig({
   server: {
-    port: 3000,
+    base: "/qr-code/",
+    port: 9090,
     open: true,
   },
 
